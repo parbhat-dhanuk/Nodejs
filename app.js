816ce455b6ac9ae3568da3607f0a4a6843cdf2c8
parbhat-dhanuk <parbhatdhanuk3@gmail.com>
2024-09-11 23:05:40 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("running in port 3000")
+})
